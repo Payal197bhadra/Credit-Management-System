56edb08313c5d58fe36951406bfb36ed67e38d4b
Payal197bhadra <46824237+Payal197bhadra@users.noreply.github.com>
2019-06-18 21:05:39 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+DEFINE ('DB_USER','root');
+DEFINE ('DB_PSWD','');
+DEFINE ('DB_HOST','localhost:3308');
+DEFINE ('DB_NAME','credit management system');
+
+$mysqli=mysqli_connect(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+
+
+?>
